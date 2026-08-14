24190234fdfb3eb86b2a767ccb5b9941eaf596cf
7hfrs9fgn6-ux <7hfrs9fgn6@privaterelay.appleid.com>
2026-08-14 14:52:13 +0800

    Create __init__.py

---
+++ scrapers/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""P1阶段：爬虫降级模块"""
+
+from .base_scraper import BaseScraper
+from .commodity_scraper import CommodityScraper, scrape_commodities
+from .macro_scraper import MacroScraper, scrape_macro
+from .sector_scraper import SectorScraper, scrape_sectors
+
+__all__ = [
+    'BaseScraper',
+    'CommodityScraper',
+    'scrape_commodities',
+    'MacroScraper',
+    'scrape_macro',
+    'SectorScraper',
+    'scrape_sectors',
+]
